0621bd6119d96cd73d9cf1d38a6d0bb472ec2be3
nachodiba210 <nachodibarrart@gmail.com>
2023-12-14 17:52:35 -0300

    before save not after save

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -6,10 +6,10 @@ class UserProfile < ApplicationRecord
   validates :given_name, :family_name, :sub, presence: true
   validates_uniqueness_of :sub, message: 'must be unique'
 
-  after_save :add_name
+  before_save :add_name
 
   def add_name
-    self.update(name: "#{given_name} #{family_name}")
+    self.name = "#{given_name} #{family_name}"
   end
 
   def self.ransackable_attributes(auth_object = nil)
